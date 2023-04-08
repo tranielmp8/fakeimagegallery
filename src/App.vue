bbfd5acd350918c77fc5808a2f52e031d15a316d
Traniel Pride <tranielmp8@gmail.com>
2023-04-08 09:54:28 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Art />
+  </div>
+</template>
+
+<script setup>
+import Art from './components/Art.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Art.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="container">
+      <div v-for="image in images" :key="image.id" >
+        {{ image.title }}
+        {{ image.printPrice }}
+        <img :src="image.img" alt="Tie Art">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+  export default {
+    setup(){
+      const images = ref([
+        {
+          "id": 1,
+          "title": "Demon Slayer",
+          "description": "just something to talk about. cool stuff",
+          "printPrice": "$500.00",
+          "uniquePrice": "$1250.00",
+          "img": '/src/assets/images/demon_slayer.jpg'
+        },
+        {
+          "id": 2,
+          "title": "Demon Slayer",
+          "description": "just something to talk about. cool stuff",
+          "printPrice": "$500.00",
+          "uniquePrice": "$1250.00",
+          "img": '/src/assets/images/luffy.jpg'
+        },
+        {
+          "id": 3,
+          "title": "Demon Slayer",
+          "description": "just something to talk about. cool stuff",
+          "printPrice": "$500.00",
+          "uniquePrice": "$1250.00",
+          "img": 'src/assets/images/bleach.jpg'
+        },
+        {
+          "id": 4,
+          "title": "Naruto",
+          "description": "just something to talk about. cool stuff",
+          "printPrice": "$500.00",
+          "uniquePrice": "$1250.00",
+          "img": './public/naruto.jpg'
+        },
+      ])
+
+      return {images}
+    }
+  }
+</script>
